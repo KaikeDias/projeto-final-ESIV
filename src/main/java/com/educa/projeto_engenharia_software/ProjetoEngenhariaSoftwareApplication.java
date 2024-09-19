532b3058c7dd1e67853a2638ffb16bcb803159c6
Kaike <kaikedias98@gmail.com>
2024-09-18 21:20:40 -0300

    initial commit

---
+++ src/main/java/com/educa/projeto_engenharia_software/ProjetoEngenhariaSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.educa.projeto_engenharia_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEngenhariaSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoEngenhariaSoftwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educa/projeto_engenharia_software/ProjetoEngenhariaSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educa.projeto_engenharia_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEngenhariaSoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
